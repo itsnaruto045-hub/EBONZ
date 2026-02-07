7329ed02fb8cc5f0893c3938ac6ce409c4afc1dd
itsnaruto045-hub <itsnaruto045@gmail.com>
2026-02-07 10:07:12 +0600

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,7 @@
 import pg from 'pg';
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://neondb_owner:npg_HQtidgOWF5Z4@ep-curly-cake-a1l5cs9u-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: { rejectUnauthorized: false }
 });
 
